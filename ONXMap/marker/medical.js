6f41d0ff36a4d4f5eaf7373f18b51fab3944213e
Solaris <12967322+Solaris333@users.noreply.github.com>
2024-04-03 02:01:43 -0400

    Again

---
+++ ONXMap/marker/medical.js
@@ -0,0 +1,27 @@
+var medical = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Mount Zonah Medical Center",
+                "name": "Mount Zonah Medical Center"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [107.984375,-179.94921875]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Sandy Shores Hospital",
+                "name": "Sandy Shores Hospital"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [151.65625,-97.265625]
+            }
+        }
+    ]
+};
---
+++ ONXMap/marker/police.js
@@ -0,0 +1,93 @@
+var police = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Vespucci Police Department",
+                "name": "Vespucci Police Department"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [94.2734375,-189.72265625]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Del Perro Police Department",
+                "name": "Del Perro Police Department"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [85.0625,-192.578125]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Vinewood Police Department",
+                "name": "Vinewood Police Department"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [129.21875,-172.21875]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Sandy Shores Sheriff Department",
+                "name": "Sandy Shores Sheriff Department"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [154.95703125,-96.62109375]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Grapeseed Sheriff Department",
+                "name": "Grapeseed Sheriff Department"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [161.9765625,-74.7109375]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Bolingbroke Penitentiary",
+                "name": "Bolingbroke Penitentiary"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [155.4296875,-119.515625]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Highway Patrol Office",
+                "name": "Highway Patrol Office"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [169.1171875,-180.640625]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Park Ranger Station",
+                "name": "Park Ranger Station"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [86.73828125,-70.01171875]
+            }
+        }
+    ]
+};

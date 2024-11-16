145774ba73ecbe11315cf7b985fc5c4295fa3e1d
Leigon <30293714+Leigon@users.noreply.github.com>
2024-11-16 22:57:01 +0000

    Update rv.js

--- ONXMapFIBONLY/marker/rv.js
+++ ONXMapFIBONLY/marker/rv.js
@@ -13,6 +13,11 @@ var rv = {
     ]
 };
 
+function isValidString(str1)
+{
+  return str1 != null && typeof str1 === "string" && str1.length > 0;
+}
+
 function fetchRV() {
  
 // Fetch the CSV file and add markers to the map
@@ -20,19 +25,20 @@ Papa.parse(rvSheetUrl , {
     download: true,
     header: true,
     complete: function(results) {
-        for( var i = 0; i < results.data.length && i < 10; i++ )
-        {
-            var lat = parseFloat(results.data[i].Latitude);
-            var lon = parseFloat(results.data[i].Longitude);
-            var stateId = parseInt(results.data[i]["State ID"]);
-            var owner = results.data[i].Owner;
-            var plate = results.data[i].Plate;
-            var picture = results.data[i].Picture;
+        data.forEach(row => {
+            var latitude = parseFloat(row.Latitude);
+            var longitude = parseFloat(row.Longitude);
+            var stateId = parseInt(row["State ID"]);
+            var owner = row.Owner;
+            var plate = row.Plate;
+            var picture = row.Picture;
 
-            rv.features[0].features[ i ] = {
+         if( isValidString(owner) && isValidString(plate) && isValidString(picture) && !isNaN( latitude ) && !isNaN( longitude ) && !isNaN( stateId ) )
+         {
+            rv.features[0].features.push({
                     "type": "Feature",
                     "properties": {
-                        "id": lat  + " " +  lon  + " " +  owner + " " + "(ID: " + stateId + ") - " + plate,
+                        "id": latitude  + " " +  longitude  + " " +  owner + " " + "(ID: " + stateId + ") - " + plate,
                         "name": owner,
                         "external_id": picture,
                         "image_link": picture,
@@ -41,12 +47,13 @@ Papa.parse(rvSheetUrl , {
                     "geometry": {
                         "type": "Point",
                         "coordinates": [
-                            lat,
-                            lon
+                            latitude,
+                            longitude
                         ]
                     }
-                };
-        }
+                });
+        });
+    }
         dataSynced();
     }
 });

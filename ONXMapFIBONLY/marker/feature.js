92f1ac41d5c339e7a1a0a0ab1241d6520fcdf8ef
Leigon <30293714+Leigon@users.noreply.github.com>
2024-11-24 19:48:36 +0000

    Update and rename rv.js to feature.js

---
+++ ONXMapFIBONLY/marker/feature.js
@@ -0,0 +1,18 @@
+var features = new Map();
+
+var featureData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "features": [
+                {
+                }
+            ]
+        }
+    ]
+};
+
+function isValidString(str1)
+{
+  return str1 != null && typeof str1 === "string" && str1.length > 0;
+}

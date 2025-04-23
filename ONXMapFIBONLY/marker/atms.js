1bbb7216a44c45e67241a579499528fbcc3690b3
Solaris <12967322+Solaris333@users.noreply.github.com>
2025-04-22 22:54:48 -0400

    ATM Part 2

--- ONXMapFIBONLY/marker/atms.js
+++ ONXMapFIBONLY/marker/atms.js
@@ -239,5 +239,383 @@ var atms = {
                 "coordinates": [ 102.7890625,-189.4296875 ]
             }
         },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Binco",
+                "name": "Binco",
+                "external_id": "https://i.imgur.com/eFNQjfT.jpeg",
+                "image_link": "https://i.imgur.com/eFNQjfT.jpeg",
+                "description": "Binco, South Rockford Dr., Vespucci Canals"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 100.359375,-194.9921875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Mr Spoke's",
+                "name": "Mr Spoke's",
+                "external_id": "https://i.imgur.com/gB8eTbk.jpeg",
+                "image_link": "https://i.imgur.com/gB8eTbk.jpeg",
+                "description": "Mr Spoke's, Bay City Ave., Vespucci Beach"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 94.625,-207.546875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Deane's World",
+                "name": "Deane's World",
+                "external_id": "https://i.imgur.com/8PrJXz8.jpeg",
+                "image_link": "https://i.imgur.com/8PrJXz8.jpeg",
+                "description": "Deane's World, Magellan Ave., Vespucci Beach"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 91.875,-202.578125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Xero Gas #1",
+                "name": "Xero Gas #1",
+                "external_id": "https://i.imgur.com/BUp5AJX.jpeg",
+                "image_link": "https://i.imgur.com/BUp5AJX.jpeg",
+                "description": "Xero Gas, Del Perro Fwy., Pacific Bluffs"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 74.1640625,-179.03125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Xero Gas #2",
+                "name": "Xero Gas #2",
+                "external_id": "https://i.imgur.com/0kIOibH.jpeg",
+                "image_link": "https://i.imgur.com/0kIOibH.jpeg",
+                "description": "Xero Gas, Del Perro Fwy., Pacific Bluffs"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 74.3125,-179.1640625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "LOMBank",
+                "name": "LOMBank",
+                "external_id": "https://i.imgur.com/o9cnFaQ.jpeg",
+                "image_link": "https://i.imgur.com/o9cnFaQ.jpeg",
+                "description": "LOMBank, Boulevard Del Perro, Del Perro"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 85.03125,-183.890625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Fleeca Bank",
+                "name": "Fleeca Bank",
+                "external_id": "https://i.imgur.com/SgxJnBy.jpeg",
+                "image_link": "https://i.imgur.com/SgxJnBy.jpeg",
+                "description": "Fleeca Bank, Prosperity St., Del Perro"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 89.296875,-182.96875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Bahamma Mama's",
+                "name": "Bahamma Mama's",
+                "external_id": "https://i.imgur.com/t8QdAOC.jpeg",
+                "image_link": "https://i.imgur.com/t8QdAOC.jpeg",
+                "description": "Bahamma Mama's, Marathon Ave., Del Perro"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 88.71875,-184.78125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Maze Bank of Los Santos",
+                "name": "Maze Bank of Los Santos",
+                "external_id": "https://i.imgur.com/bRJK1y8.jpeg",
+                "image_link": "https://i.imgur.com/bRJK1y8.jpeg",
+                "description": "Maze Bank of Los Santos, Bay City Ave., Del Perro"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 90.4375,-190.265625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Fleeca Bank",
+                "name": "Fleeca Bank",
+                "external_id": "https://i.imgur.com/XBR05GA.jpeg",
+                "image_link": "https://i.imgur.com/XBR05GA.jpeg",
+                "description": "Fleeca Bank, Boulevard Del Perro, Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 92.59375,-179.4375 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Fleeca Bank",
+                "name": "Fleeca Bank",
+                "external_id": "https://i.imgur.com/wTW7frb.jpeg",
+                "image_link": "https://i.imgur.com/wTW7frb.jpeg",
+                "description": "Fleeca Bank, Boulevard Del Perro, Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 87.90625,-176.78125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Tivoli Theater",
+                "name": "Tivoli Theater",
+                "external_id": "https://i.imgur.com/SgKKIpI.jpeg",
+                "image_link": "https://i.imgur.com/SgKKIpI.jpeg",
+                "description": "Tivoli Theater, Cougar Ave., Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 87.90625,-176.78125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "International Online Unlimited",
+                "name": "International Online Unlimited",
+                "external_id": "https://i.imgur.com/EbRWEY2.jpeg",
+                "image_link": "https://i.imgur.com/EbRWEY2.jpeg",
+                "description": "International Online Unlimited, Cougar Ave., Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 88.265625,-174.640625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "International Online Unlimited",
+                "name": "International Online Unlimited",
+                "external_id": "https://i.imgur.com/VbpDBef.jpeg",
+                "image_link": "https://i.imgur.com/VbpDBef.jpeg",
+                "description": "International Online Unlimited, Dorset Dr., Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 99.875,-179.21875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Leopolds",
+                "name": "Leopolds",
+                "external_id": "https://i.imgur.com/kiWPbD8.jpeg",
+                "image_link": "https://i.imgur.com/kiWPbD8.jpeg",
+                "description": "Leopolds, Dorset Dr., Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 102.59375,-181.328125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "City Hall",
+                "name": "City Hall",
+                "external_id": "https://i.imgur.com/mB1JXVU.jpeg",
+                "image_link": "https://i.imgur.com/mB1JXVU.jpeg",
+                "description": "City Hall, Carcer Way, Burton"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 106.171875,-176.6875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "LOMBank",
+                "name": "LOMBank",
+                "external_id": "https://i.imgur.com/039FWbl.jpeg",
+                "image_link": "https://i.imgur.com/039FWbl.jpeg",
+                "description": "LOMBank, Mad Wayne Thunder Dr., Rockford Hills"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 99.59375,-176.546875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Cluckin' Bell",
+                "name": "Cluckin' Bell",
+                "external_id": "https://i.imgur.com/RTA6CDu.jpeg",
+                "image_link": "https://i.imgur.com/RTA6CDu.jpeg",
+                "description": "Cluckin' Bell, Las Lagunas Blvd., Burton"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 114.3125,-178.03125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Liquor Store",
+                "name": "Liquor Store",
+                "external_id": "https://i.imgur.com/AIRco1w.jpeg",
+                "image_link": "https://i.imgur.com/AIRco1w.jpeg",
+                "description": "Liquor Store, Las Lagunas Blvd., Burton"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 116.25,-174.5625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Pop's Pills",
+                "name": "Pop's Pills",
+                "external_id": "https://i.imgur.com/TXp3rkG.jpeg",
+                "image_link": "https://i.imgur.com/TXp3rkG.jpeg",
+                "description": "Pop's Pills, Spanish Ave., Downtown Vinewood"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 119.3125,-172.65625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Fred's Store",
+                "name": "Fred's Store",
+                "external_id": "https://i.imgur.com/Jii92l8.jpeg",
+                "image_link": "https://i.imgur.com/Jii92l8.jpeg",
+                "description": "Fred's Store, Vinewood Blvd., Downtown Vinewood"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 123.21875,-169.7109375 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Slaughter, Slaughter & Slaughter",
+                "name": "Slaughter, Slaughter & Slaughter",
+                "external_id": "https://i.imgur.com/X1KzkjB.jpeg",
+                "image_link": "https://i.imgur.com/X1KzkjB.jpeg",
+                "description": "Slaughter, Slaughter & Slaughter, San Andreas Ave., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 112.125,-186.96875 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Infinity Apartments",
+                "name": "Infinity Apartments",
+                "external_id": "https://i.imgur.com/mcXPZcB.jpeg",
+                "image_link": "https://i.imgur.com/mcXPZcB.jpeg",
+                "description": "Infinity Apartments, Peaceful St., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 111.984375,-187.328125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Maison Ricard",
+                "name": "Maison Ricard",
+                "external_id": "https://i.imgur.com/MqmaAKZ.jpeg",
+                "image_link": "https://i.imgur.com/MqmaAKZ.jpeg",
+                "description": "Maison Ricard, San Andreas Ave., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 119.75,-189.5625 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "FIB Building, Lower",
+                "name": "FIB Building, Lower",
+                "external_id": "https://i.imgur.com/thmFoER.jpeg",
+                "image_link": "https://i.imgur.com/thmFoER.jpeg",
+                "description": "FIB Building, Lower, San Andreas Ave., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 119.546875,-188.53125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "LOMBank",
+                "name": "LOMBank",
+                "external_id": "https://i.imgur.com/zEOm22m.jpeg",
+                "image_link": "https://i.imgur.com/zEOm22m.jpeg",
+                "description": "LOMBank, Power St., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 119.546875,-188.53125 ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "Premium Deluxe Motorsport",
+                "name": "Premium Deluxe Motorsport",
+                "external_id": "https://i.imgur.com/f8Ou01n.jpeg",
+                "image_link": "https://i.imgur.com/f8Ou01n.jpeg",
+                "description": "Premium Deluxe Motorsport, Power St., Pillbox Hill"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [ 116.609375,-195.26953125 ]
+            }
+        },
     ]
 };
